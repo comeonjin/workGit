8d4c1abba6041c99708b0d6feaa6c203d6a8646b
zhangjin <zhangjin@gaosiedu.com>
2018-03-17 21:57:37 +0800

    add vue-loader

--- src/main.js
+++ src/main.js
@@ -1,2 +1,8 @@
 import Vue from 'vue'
 import vueRouter from 'vue-router'
+
+//创建vue根实例对象
+new Vue({
+    el: '#app',
+    
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-    entry: './src/main.js'
+    entry: './src/main.js',
+    output: {
+        path: './dist',
+        filename: '[name].js'
+    }
 }
